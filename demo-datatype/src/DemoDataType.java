b1acc788d4fe329ed3e7df8ec10c8cf9ffd30b07
chris.wong <12019445@life.hkbu.edu.hk>
2025-04-12 21:44:36 +0800

    renew

---
+++ demo-datatype/src/DemoDataType.java
@@ -0,0 +1,71 @@
+public class DemoDataType {
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    // Variable
+    // ! integer (+ve -ve)
+    // ! int
+    // "="-> asssignment 
+    // from right to left 
+    // x and y are variable
+    int x = 3;
+    //put 7 into y
+    int y = 7;
+
+    System.out.println("x");
+    System.out.println(x);
+    System.out.println("y");
+    System.out.println(y);
+    
+    //re-assign value 10 into variable x
+    x = 10;
+    System.out.println(x);
+    
+    y = 100; 
+    System.out.println(y);
+    // shortcut: sysoyt -> System.out.println();
+    int age = 65; 
+    int salary = 20000; 
+    // variable name convention
+    // 1. must started with a-z
+    // 2. Camel case
+    int tutorName = 3; 
+
+System.out.println(tutorName);
+// Declaration for variable type
+int a;
+// Same variable name can be declared once only
+// Assignment
+a = 4;
+System.out.println(a);
+
+// declaration and assignment
+int w =10;
+
+    //2 ! double 
+    double d1 = 4.45;
+    System.out.println(d1);
+
+    double d3;
+    d3 = 10.4; 
+    System.out.println(d3);
+    
+    // + - * /
+    int x2 = 6 + 1; 
+System.out.println(x2);
+    int x3 = 9;
+    // ! put x3 value into
+    x2 = x3; 
+    System.out.println(x2);
+    System.out.println(x3);
+
+    int x4 = 10; 
+    x2 = 7 + x4;
+System.out.println(x2);
+
+//add 3 to itself
+    x2 = x2 + 8;
+    System.out.println(x2);
+
+
+  }
+}
